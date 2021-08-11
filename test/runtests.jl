a7c00958b737e127dd49397d85ff98cee4e1261d
Inazuma110 <c011703534@edu.teu.ac.jp>
2021-08-11 17:30:32 +0900

    add: test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using IterativelyHypergraphEmbedding
 using SimpleHypergraphs
+using LinearAlgebra
 using Test
+
 
 @testset "IterativelyHypergraphEmbedding.jl" begin
   h = Hypergraph{Int, Int}(4, 0)
@@ -11,11 +13,11 @@ using Test
   @test size(hn_vec) == (dims, nhv(h))
   @test size(he_vec) == (dims, nhe(h))
 
-  h = Hypergraph{Int, Int}(4, 0)
-  add_hyperedge!(h, vertices=Dict(1=>1, 2=>1, 3=>1))
-  add_hyperedge!(h, vertices=Dict(3=>1, 4=>1))
-  dims = 10
-  hn_vec, he_vec = iteratively_hypergraph_embedding(h, dims)
-  @test size(hn_vec) == (dims, nhv(h))
-  @test size(he_vec) == (dims, nhe(h))
+  for vec in eachcol(hn_vec)
+    @test abs(norm(vec) - 1) < 1e-8
+  end
+  for vec in eachcol(he_vec)
+    @test abs(norm(vec) - 1) < 1e-8
+  end
+
 end
