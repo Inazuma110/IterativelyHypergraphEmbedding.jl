75cbf10ad69a79c35195e6ca3ff2cf98985cd72b
Documenter.jl <documenter@juliadocs.github.io>
2022-01-20 08:09:55 +0000

    build based on 57c8a69

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = IterativelyHypergraphEmbedding","category":"page"},{"location":"#IterativelyHypergraphEmbedding","page":"Home","title":"IterativelyHypergraphEmbedding","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for IterativelyHypergraphEmbedding.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [IterativelyHypergraphEmbedding]","category":"page"}]
+}
